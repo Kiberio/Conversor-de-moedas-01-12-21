65219424a691f9fcc4ca6f3a5ce7f489b79fc8ab
Kiberio <dhiroshi3@gmail.com>
2021-11-30 14:40:49 -0300

    Cometarios

--- Conversor de moedas/Program.cs
+++ Conversor de moedas/Program.cs
@@ -12,21 +12,22 @@ namespace Conversor_de_moedas
     {
         static void Main(string[] args)
         {
-            //
+            //Solicitando informações para o usuario
             Console.WriteLine("Qual é a cotação do dólar ? ");
             double cotacao = double.Parse(Console.ReadLine());
             Console.WriteLine("Quantos dólares você ira comprar? ");
             double CDolar = double.Parse(Console.ReadLine());
             
-            
+            //Calculo do Valor a ser pago e imprimir para o usuario
             double Vpago = cotacao * CDolar;
             Console.WriteLine( $"Valor a ser pago em reais = ${Vpago} ");
             
-
+            //Calculo Para Valor total com IOF
             double IOF = 4.08;
             double IOFC = Vpago*4.08 / 100;
-            
             double Valortot = Vpago + IOFC;
+
+            //Imprimir os valores do IOF e adicionar no valor de compra das moedas
             Console.WriteLine($"Valor a ser pago de IOF = {IOF}% ");
             Console.WriteLine("ATENÇÃO: VALOR DE IOF PARA PESSOA FISICA DO DIA 20/09/2021");
             Console.WriteLine($"Valor total da compra com IOF = ${Valortot} ");
